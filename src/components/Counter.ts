4ab14d4a6e37efa540e680d2cc704ba1c94ad4e4
B1lol-dev <sardorovichgamer@gmail.com>
2025-04-27 16:37:47 +0500

    added table to counter

--- src/components/Counter.ts
+++ src/components/Counter.ts
@@ -25,11 +25,17 @@ export const Counter = (count: number): string => {
   }, 0);
 
   return /*html*/ `
-    <div class="min-h-screen flex flex-col items-center justify-center bg-blue-950 snap-center">
-      <button type="button" id="counter_button">
+    <div class="min-h-screen flex flex-col items-center justify-center bg-blue-950 snap-center relative">
+      <button type="button" id="counter_button" class="z-1 shadow-md scale-150">
         <span class="counter_button_back"></span>
         <span class="counter_button_front"><p class="text-white">${count}</p></span>
       </button>
+      <div class="absolute h-[600px] w-[750px] skew-x-20 bg-amber-700 bottom-40">
+        <div class="h-[calc(100%+170px)] w-[50px] bg-amber-700 -skew-x-20 absolute top-0 -translate-x-[140px]"></div>
+        <div class="h-[calc(100%+170px)] w-[50px] bg-amber-700 -skew-x-20 absolute top-0 right-0 -translate-x-[140px]"></div>
+        <div class="h-[calc(100%+170px)] w-[50px] bg-amber-700 -skew-x-20 absolute bottom-0 translate-y-full -translate-x-[140px]"></div>
+        <div class="h-[calc(100%+170px)] w-[50px] bg-amber-700 -skew-x-20 absolute bottom-0 right-0 translate-y-full -translate-x-[140px]"></div>
+      </div>
     </div>
     `;
 };
